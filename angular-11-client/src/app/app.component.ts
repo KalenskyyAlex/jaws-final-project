ddd14bbd9a298d3b29173ee85c324425a899e7a1
KalenskyyAlex <oleksandrkalenskij7@gmail.com>
2023-06-30 01:08:50 +0200

    previous commit quickfixes

--- angular-11-client/src/app/app.component.ts
+++ angular-11-client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ConfigStateService } from './services/config-state.service';
 import { Route, Router } from '@angular/router';
+import { platform } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,6 @@ export class AppComponent {
   logOut(): void {
     this.config.storeConfig.inSystem = false;
     this.config.storeConfig.userHash = "";
-    this.router.navigate(['/']);
+    window.location.reload();
   }
 }
